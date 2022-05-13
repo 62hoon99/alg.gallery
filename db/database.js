307e4bc46dad7021232f1fdc09742514191d4ce6
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-05-13 15:49:18 +0900

    보안 완료_1

---
+++ db/database.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const db = {
+    "development": {
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.password,
+        database: process.env.database
+    }
+}
+
+const dbcon = {
+    init: () => {
+        return mysql.createConnection(db.development);
+    },
+    salt: () => {
+        return process.env.salt;
+    },
+    secretSessionKey: () => {
+        return process.env.secretSessionKey;
+    }
+}
+
+module.exports = dbcon;
