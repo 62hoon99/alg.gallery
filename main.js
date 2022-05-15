e479810f12a2dc05a1e3ad055ed7eb4416420468
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-05-15 19:54:06 +0900

    res-form 수정

--- main.js
+++ main.js
@@ -30,14 +30,6 @@ app.use('/users', usersRouter);
 app.use('/captcha', captchaRouter);
 app.use('/post', postRouter);
 
-app.get('/signup/success', (req, res) => { // 로그인 성공시 리디렉션되는 페이지
-    res.status(200).send("so good!");
-})
-
-app.get('/signup/failure', (req, res) => { // 로그인 실패시 리디렉션되는 페이지
-    res.status(400).send("so bad!");
-})
-
 app.use(function (req, res, next) {
     res.status(404).send('Sorry cant find that!');
 });
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../db/database').init();
 const hashing = require('../config/hashing');
 const salt = require('../db/database').salt();
+const res_form = require('../lib/res_from');
 
 module.exports = (passport) => {
     router.post('/signup', (req, res) => {
@@ -10,16 +11,17 @@ module.exports = (passport) => {
         const hashValue = hashing.enc(userInfo.userid, userInfo.password, salt);
         db.query('INSERT INTO users VALUES(?, ?);', [userInfo.userid, hashValue], (error, data) => {
             if (error) {
-                res.status(400).end();
+                res.status(400).json(res_form.fail());
+            }
+            else {
+                res.status(200).json(res_form.success());
             }
         });
-
-        res.status(200).end();
     });
 
     router.post('/signin', passport.authenticate('local', {
-        successRedirect: '/signup/success', // 로그인 성공시 리디렉션되는 페이지
-        failureRedirect: '/signup/failure', // 로그인 실패시 리디렉션되는 페이지
+        successRedirect: '/users/signin/success', // 로그인 성공시 리디렉션되는 페이지
+        failureRedirect: '/users/signin/failure', // 로그인 실패시 리디렉션되는 페이지
         failureFlash: true,
         successFlash: true
     }));
@@ -27,19 +29,31 @@ module.exports = (passport) => {
     router.post('/signout', (req, res) => {
         req.logout();
         req.session.destroy((err) => {
-            res.redirect('/signup/success'); // 로그아웃 성공시 리디렉션되는 페이지
+            res.redirect('/users/signout/success'); // 로그아웃 성공시 리디렉션되는 페이지
         });
     });
 
     router.get('/id/overlap', (req, res) => {
         const userInfo = req.body;
         db.query('SELECT * FROM users WHERE userid = ?', [userInfo.userid], (error, data) => {
-            if (Object.keys(data).length) {
-                res.status(400).send();
+            if (data[0] === undefined) {
+                res.status(200).json(res_form.success());
             } else {
-                res.status(200).send();
+                res.status(400).json(res_form.fail());
             }
         });
+    });
+
+    router.get('/signin/success', (req, res) => { // 로그인 성공시 리디렉션되는 페이지
+        res.status(200).json(res_form.success());
+    });
+
+    router.get('/signin/failure', (req, res) => { // 로그인 실패시 리디렉션되는 페이지
+        res.status(400).json(res_form.fail());
+    });
+
+    router.get('/signout/success', (req, res) => { // 로그아웃 성공시 리디렉션되는 페이지
+        res.status(200).json(res_form.success());
     });
 
     return router;
