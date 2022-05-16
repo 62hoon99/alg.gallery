df8061661849025977a7bc111d13656aad139ad6
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-05-16 22:28:44 +0900

    likes api_0

--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const db = require('../db/database').init();
 const moment = require('moment');
 const res_form = require('../lib/res_from');
+const error_form = (error) => {
+    let form = res_form.fail();
+    form.errors = error;
+    return form;
+}
 
 module.exports = () => {
     router.post('/registration', (req, res) => {
@@ -10,9 +15,7 @@ module.exports = () => {
         const postDate = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
         db.query('INSERT INTO post VALUES(?, ?, ?, ?, ?, ?, ?, ?);', [null, postInfo.userid, postInfo.algCode, postInfo.text, postInfo.tag1, postInfo.tag2, postInfo.tag3, postDate], (error, data) => {
             if (error) {
-                let form = res_form.fail();
-                form.errors = error;
-                res.status(400).json(form);
+                res.status(400).json(error_form(error));
             }
             else {
                 let form = res_form.success();
@@ -26,15 +29,27 @@ module.exports = () => {
 
     router.get('/modal', (req, res) => {
         const postid = req.body.postid;
-        db.query('SELECT * FROM post WHERE postid = ?', [postid], (error, data) => {
+        db.query('SELECT * FROM post WHERE postid = ?;', [postid], (error, data) => {
             if (error) {
-                let form = res_form.fail();
-                form.errors = error;
-                res.status(400).json(form);
+                res.status(400).json(error_form(error));
             }
             else if (data[0] === undefined) {
                 let form = res_form.fail();
                 res.status(404).json(form);
+            }
+            else {
+                let form = res_form.success();
+                form.data = data[0];
+                res.status(200).json(form);
+            }
+        });
+    });
+
+    router.get('/sort/lang', (req, res) => {
+        const lang = req.body.lang;
+        db.query('SELECT * FROM post WHERE tag1 = ?;', [lang], (error, data) => {
+            if (error) {
+                res.status(400).json(error_form(error));
             }
             else {
                 let form = res_form.success();
@@ -44,20 +59,59 @@ module.exports = () => {
         });
     });
 
-    router.get('/sort/lang', (req, res) => {
-        const lang = req.body.lang;
-        db.query('SELECT * FROM post WHERE tag1 = ?', [lang], (error, data) => {
-            if (error) {
-                let form = res_form.fail();
-                form.errors = error;
-                res.status(400).json(form);
-            }
-            else {
-                let form = res_form.success();
-                form.data = data;
-                res.status(200).json(form);
-            }
-        });
+    router.post('/likes/click', (req, res) => {
+        if (req.session === undefined || req.session.passport === undefined) { // 비인증 상태인 유저가 좋아요를 누른 경우
+            let form = res_form.fail();
+            form.errors = "비인증 상태 - 로그인 필요"
+            res.status(401).json(form);
+        }
+        else {
+            const postid = req.body.postid;
+            const userid = req.session.passport.user.userid;
+            db.query('SELECT * FROM likes WHERE postid = ? and userid = ?;', [postid, userid], (error, data) => {
+                if (error) {
+                    res.status(400).json(error_form(error));
+                }
+                else if (data[0] === undefined) { // 사용자가 해당 post에 좋아요 누른적이 없는 경우
+                    likes_insert(postid, userid);
+                    likes_count(postid, 1);
+                }
+                else { // 사용자가 해당 post에 좋아요 누른적이 있는 경우
+                    likes_delete(postid, userid);
+                    likes_count(postid, 0);
+                }
+            });
+        }
+
+        const likes_insert = (postid, userid) => {
+            db.query('INSERT INTO likes values(?, ?);', [postid, userid], (error, data) => {
+                if (error) {
+                    res.status(400).json(error_form(error));
+                }
+            });
+        };
+
+        const likes_delete = (postid, userid) => {
+            db.query('DELETE FROM likes WHERE postid = ? and userid = ?', [postid, userid], (error, data) => {
+                if (error) {
+                    res.status(400).json(error_form(error));
+                }
+            });
+        };
+
+        const likes_count = (postid, status) => {
+            db.query('SELECT COUNT(*) AS count FROM likes WHERE postid = ?', [postid], (error, data) => {
+                if (error) {
+                    res.status(400).json(error_form(error));
+                }
+                else {
+                    let form = res_form.success();
+                    form.data = data[0];
+                    form.data.status = status; // 좋아요면 1 아니면 0
+                    res.status(200).json(form);
+                }
+            });
+        };
     });
 
     return router;
