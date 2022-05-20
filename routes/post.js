e128fd81f25c8522151f84cf9a564c3e77869e8d
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-05-20 14:30:47 +0900

    post api_2

--- routes/post.js
+++ routes/post.js
@@ -25,7 +25,7 @@ module.exports = () => {
 
     router.get('/modal', (req, res) => {
         const postid = req.query.postid;
-        db.query('SELECT * FROM post WHERE postid = ?;', [postid], (error, data) => {
+        db.query('select * from post, (select count(*) as count, postid from likes group by postid) l_table where post.postid = l_table.postid and post.postid = ?;', [postid], (error, data) => {
             if (error) {
                 res.status(400).json(res_form.error(error));
             }
@@ -35,7 +35,7 @@ module.exports = () => {
             }
             else {
                 let form = res_form.success();
-                form.data = data[0];
+                form.data = data;
                 res.status(200).json(form);
             }
         });
@@ -73,22 +73,20 @@ module.exports = () => {
         if (req.session === undefined || req.session.passport === undefined) { // 비인증 상태인 유저가 좋아요를 누른 경우
             let form = res_form.fail();
             form.errors = "비인증 상태 - 로그인 필요"
-            res.status(401).json(form);
+            return res.status(401).json(form);
         }
         else {
             const postid = req.body.postid;
             const userid = req.session.passport.user.userid;
             db.query('SELECT * FROM likes WHERE postid = ? and userid = ?;', [postid, userid], (error, data) => {
                 if (error) {
-                    res.status(400).json(res_form.error(error));
+                    return res.status(400).json(res_form.error(error));
                 }
                 else if (data[0] === undefined) { // 사용자가 해당 post에 좋아요 누른적이 없는 경우
                     likes_insert(postid, userid);
-                    likes_count(postid, 1);
                 }
                 else { // 사용자가 해당 post에 좋아요 누른적이 있는 경우
                     likes_delete(postid, userid);
-                    likes_count(postid, 0);
                 }
             });
         }
@@ -96,7 +94,10 @@ module.exports = () => {
         const likes_insert = (postid, userid) => {
             db.query('INSERT INTO likes values(?, ?);', [postid, userid], (error, data) => {
                 if (error) {
-                    res.status(400).json(res_form.error(error));
+                    return res.status(400).json(res_form.error(error));
+                }
+                else {
+                    likes_count(postid, 1);
                 }
             });
         };
@@ -104,7 +105,10 @@ module.exports = () => {
         const likes_delete = (postid, userid) => {
             db.query('DELETE FROM likes WHERE postid = ? and userid = ?', [postid, userid], (error, data) => {
                 if (error) {
-                    res.status(400).json(res_form.error(error));
+                    return res.status(400).json(res_form.error(error));
+                }
+                else {
+                    likes_count(postid, 0);
                 }
             });
         };
@@ -112,13 +116,13 @@ module.exports = () => {
         const likes_count = (postid, status) => {
             db.query('SELECT COUNT(*) AS count FROM likes WHERE postid = ?', [postid], (error, data) => {
                 if (error) {
-                    res.status(400).json(res_form.error(error));
+                    return res.status(400).json(res_form.error(error));
                 }
                 else {
                     let form = res_form.success();
                     form.data = data[0];
                     form.data.status = status; // 좋아요면 1 아니면 0
-                    res.status(200).json(form);
+                    return res.status(200).json(form);
                 }
             });
         };
