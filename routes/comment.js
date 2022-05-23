19af1f09d5f542defb3ca4d58500ae41c950e10e
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-05-23 22:28:54 +0900

    commentid 추가

--- routes/comment.js
+++ routes/comment.js
@@ -10,7 +10,7 @@ module.exports = () => {
         const commentInfo = req.body;
         const commentDate = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
         const userid = req.session.passport.user.userid;
-        db.query('INSERT INTO comment VALUES(?, ?, ?, ?);', [commentInfo.postid, userid, commentInfo.text, commentDate], (error, data) => {
+        db.query('INSERT INTO comment VALUES(?, ?, ?, ?, ?);', [null, commentInfo.postid, userid, commentInfo.text, commentDate], (error, data) => {
             if (error) {
                 res.json(400, res_form.error(error));
             }
@@ -22,7 +22,7 @@ module.exports = () => {
 
     router.get('/order', (req, res) => {
         const postid = req.query.postid;
-        db.query(' SELECT postid, comment.userid, nickname, text, comment_date FROM comment, (select userid, nickname from users) u_table WHERE comment.userid = u_table.userid and postid = ? ORDER BY comment_date;', [postid], (error, data) => {
+        db.query('SELECT commentid, postid, comment.userid, nickname, text, comment_date FROM comment, (select userid, nickname from users) u_table WHERE comment.userid = u_table.userid and postid = ? ORDER BY comment_date;', [postid], (error, data) => {
             if (error) {
                 res.json(400, res_form.error(error));
             }
