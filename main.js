8f66dd76cf0bf7cde6a7a05836f68fc7727f062a
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-05-28 23:33:33 +0900

    session 수정_5

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ app.use(session({
     store: new FileStore()
 }));
 app.use(flash());
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 const passport = require('./config/passport')(app);
 const usersRouter = require('./routes/users')(passport);
